350a96abda7e877445235a06a8b060ecbacc6b31
Ammar <142759310+AmmarNabilF@users.noreply.github.com>
2024-11-21 12:32:51 +0800

    change some styles

--- navbar.php
+++ navbar.php
@@ -1,8 +1,10 @@
-    <header class="navbar">
+<header class="navbar">
         <img src="assets/logo.png" alt="">
         <a href="berandaadmin.php">
+            <a href="logout.php">
             <div>
-                <span>Log Out</span>
-            </div>
+                    <span>Log Out</span>
+                </div>
+            </a>
         </a>
     </header>
