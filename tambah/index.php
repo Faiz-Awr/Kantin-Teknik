9f2a93b81693b7412125a45e8dce2833b5d096e5
GalaxionZero <zetayu24@gmail.com>
2024-11-26 20:49:13 +0800

    now you can logout on adding and changing menu items pages

--- tambah/index.php
+++ tambah/index.php
@@ -26,7 +26,7 @@
         <img src="../assets/logo.png" alt="">
         <span class="nav-judul">Kantin Teknik</span>
         <a href="berandaadmin.php">
-            <a href="logout.php">
+            <a href="../logout.php">
                 <div>
                     <span>Keluar Akun</span>
                 </div>
